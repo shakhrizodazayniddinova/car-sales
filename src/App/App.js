b63d76455b94522e0ef564a49ebc900091984640
Your Name <shahrizodazayniddinova7@gmail.com>
2025-02-12 17:48:20 +0500

    Base project structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
+import AppRouter from "../routes/AppRouter";
+
 function App() {
   return (
     <div className="App">
+      <AppRouter />
     </div>
   );
 }
---
+++ src/routes/AppRouter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AdminDashboard from '../components/admin-panel/AdminDashboard'
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/user' index element={<AdminDashboard />}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
